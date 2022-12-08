8762b338155f574ab506a412a55f84dd6cf9e0dd
Francisco Sanchez <frango9000@gmail.com>
2022-12-08 17:31:49 +0100

    Conventions

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,8 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(other: MyDate): Int {
+        return "${this.year}${this.month}${this.dayOfMonth}".toInt() - "${other.year}${other.month}${other.dayOfMonth}".toInt()
+    }
+
 }
 
 fun test(date1: MyDate, date2: MyDate) {
